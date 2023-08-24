44a2d93f08460ff2afa54f6655530e81efe68c17
soobbb <jsb8437@naver.com>
2023-08-25 03:04:02 +0900

    1.변수

---
+++ 4장.변수/1.변수.js
@@ -0,0 +1,12 @@
+const userId = 1;
+const useName = "Soobin";
+
+const user = { id: 1, name: "Soobin" };
+
+const users = [
+  {
+    id: 1,
+    name: "Soobin",
+  },
+  { id: 2, name: "Jeon" },
+];
