c65d64d7294c8cceca2cfc2a623ce84b5f4d9c1c
soobbb <jsb8437@naver.com>
2023-09-06 00:46:53 +0900

    [study] 네임스페이스 객체

---
+++ 14장. 전역 변수의 문제점/2. 네임스페이스 객체.js
@@ -0,0 +1,15 @@
+var MYAPP = {}; // 전역 네임스페이스 객체
+
+MYAPP.name = "Jeon";
+
+console.log(MYAPP.name); // JEON
+
+
+var MYAPP = {}; // 전역 네임스페이스 객체
+
+MYAPP.person = {
+    name: "Jeon",
+    address: "Seoul"
+}
+
+console.log(MYAPP.person.name); // JEON
