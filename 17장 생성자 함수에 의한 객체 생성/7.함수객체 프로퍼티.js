355aa0fbf4a8a641026a30fc3b4d5bcec0a193fc
soobbb <jsb8437@naver.com>
2023-09-11 11:35:56 +0900

    [study] 순서변경

--- 17장 생성자 함수에 의한 객체 생성/7.함수객체 프로퍼티.js
+++
@@ -1,7 +0,0 @@
-function square(num) {
-    return num * num;
-}
-
-console.log(Object.getOwnPropertyDescriptors(square));
-
-console.log(Object.getOwnPropertyDescriptors(Object.prototype, '__proto__'));
--- 17장 생성자 함수에 의한 객체 생성/8.length 프로퍼티.js
+++
@@ -1,4 +0,0 @@
-function baz(x, y) {
-    return x * y;
-}
-console.log(baz.length); //2
