0869b80a3eda0c7448d30d6f61d0f068a047e6d6
soobbb <jsb8437@naver.com>
2023-09-13 00:18:12 +0900

    [study] instanceof 연산자

---
+++ 19장 프로토타입/13.instanceof 연산자.js
@@ -0,0 +1,11 @@
+// 생성자 함수
+function Person(name) {
+  this.name = name;
+}
+
+const me = new Person("Jeon");
+
+// Person.prototype이 me 객체의 프로토타입 체인 상에 존재하므로 true로 평가된다.
+console.log(me instanceof Person); // true
+console.log(me instanceof Object); // true
+
