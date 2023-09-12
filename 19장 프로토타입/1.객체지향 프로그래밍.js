a88333158af1617b04110343b72ba839951f853e
soobbb <jsb8437@naver.com>
2023-09-12 20:19:43 +0900

    [study] 객체지향 프로그래밍

---
+++ 19장 프로토타입/1.객체지향 프로그래밍.js
@@ -0,0 +1,24 @@
+const circle = {
+  radius: 5, // 반지름
+
+  // 원의 지름: 2r
+  getDiameter() {
+    return 2 * this.radius;
+  },
+
+  // 원의 둘레: 2πr
+  getPerimeter() {
+    return 2 * Math.PI * this.radius;
+  },
+
+  // 원의 넓이: πrr
+  getArea() {
+    return Math.PI * this.radius ** 2;
+  },
+};
+
+console.log(circle);
+
+console.log(circle.getDiameter()); // 10
+console.log(circle.getPerimeter()); // 31.41592
+console.log(circle.getAreameter()); // 78.53981
