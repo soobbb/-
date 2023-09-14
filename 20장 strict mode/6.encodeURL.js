f0d3d52dd5f9fc31ea63b1f292fd61eed2f78967
soobbb <jsb8437@naver.com>
2023-09-14 14:15:47 +0900

    [study] encodeURL

---
+++ 20장 strict mode/6.encodeURL.js
@@ -0,0 +1,5 @@
+const uri =
+  "https://www.google.com/search?sca_esv=564954462&rlz=1C5CHFA_enKR1063KR1063&sxsrf=AB5stBiV22puhrEeAoNWdlWU3MkbI7RaZw:1694591870240&q=uri+%EC%9D%B8%EC%BD%94%EB%94%A9&tbm=isch&source=lnms&sa=X&ved=2ahUKEwjCydvTjqeBAxXZxDgGHdXVBRYQ0pQJegQIDRAB&biw=1066&bih=1042&dpr=1.8#imgrc=e0V9d8NgJoji2M";
+
+const enc = encodeURI(uri);
+console.log(enc);
