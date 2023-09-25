59e488f1998ad09cbbf5641d74a2c99a3e140ac5
soobbb <jsb8437@naver.com>
2023-09-25 16:19:56 +0900

    [study] 화살표 함수

--- 26장 ES6 함수의 추가 기능/4.화살표 함수.js
+++ 26장 ES6 함수의 추가 기능/4.화살표 함수.js
@@ -1,2 +1,9 @@
 const multiply = (x, y) => x * y;
 console.log(multiply(2, 3));
+
+const create = (id, content) => ({ id, content });
+console.log(create(1, "Javascript"));
+
+// ES6
+const arr = [1, 2, 3].map((x) => x * 2);
+console.log(arr);
