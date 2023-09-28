3010e746431e929ec586cde7e2b309a344bc32c1
soobbb <jsb8437@naver.com>
2023-09-28 10:13:09 +0900

    [study] 희소 요소의 참조

---
+++ 27장 배열/8.희소 요소의 참조.js
@@ -0,0 +1,8 @@
+// 희소 배열
+const arr = [1, , 3];
+
+// 배열 arr에는 인덱스가 1인 요소가 존재하지 않는다.
+console.log(arr[0]); // 1
+console.log(arr[1]); // undefined
+console.log(arr[2]); // 3
+console.log(arr[3]); // undefined
