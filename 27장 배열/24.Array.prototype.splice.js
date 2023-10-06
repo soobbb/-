ae498dcb2b77df299c9775c19a3b7e58a8db148a
soobbb <jsb8437@naver.com>
2023-10-06 16:37:35 +0900

    [study] 배열 메서드 splice2

--- 27장 배열/24.Array.prototype.splice.js
+++ 27장 배열/24.Array.prototype.splice.js
@@ -9,3 +9,13 @@ const res = arr.splice(1, 2, 20, 30);
 console.log(res); // [2,3]
 // splice 메서드는 원본 배열을 직접 변경한다.
 console.log(arr); // [ 1, 20, 30, 4 ]
+
+const arr1 = [1, 2, 3, 4];
+// 원본 배열의 인덱스 1부터 2개의 요소를 제거한다.
+const res1 = arr1.splice(1, 2);
+
+// 원본 배열이 변경
+console.log(arr1); // [ 1, 4 ]
+
+// 제거한 요소가 배열로 리턴
+console.log(res1); // [ 2, 3 ]
