bf4af0914a6dda80f05ab2d72aa28c15e54d273c
soobbb <jsb8437@naver.com>
2023-10-06 17:42:37 +0900

    [study] slice2

---
+++ 27장 배열/28.Array.prototype.slice2.js
@@ -0,0 +1,15 @@
+const todos = [
+  { id: 1, content: "HTML" },
+  { id: 2, content: "CSS" },
+  { id: 3, content: "JS" },
+];
+
+// 얕은 복사
+const _todos = todos.slice();
+// const _todos = [...todos];
+
+// _todos와 todos는 참조 값이 다른 별개의 객체
+console.log(_todos === todos); // false
+
+// 배열 요소의 참조값이 같다. 즉, 얕은 복사
+console.log(_todos[0] === todos[0]); // true
