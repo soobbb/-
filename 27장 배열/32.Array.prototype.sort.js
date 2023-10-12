3d3e671775be61af7aa79a9585a10ad51546818e
soobbb <jsb8437@naver.com>
2023-10-12 17:30:09 +0900

    [study] sort 배열 메서드

---
+++ 27장 배열/32.Array.prototype.sort.js
@@ -0,0 +1,13 @@
+const fruits = ["Banana", "Apple", "Orange"];
+
+// 오름차순 정렬
+fruits.sort();
+
+// sort 메서드는 원본 배열을 직접 변경한다.
+console.log(fruits); // [ 'Apple', 'Banana', 'Orange' ]
+
+// 내림차순 정렬
+fruits.reverse()
+
+console.log(fruits); // [ 'Orange', 'Banana', 'Apple' ]
+
