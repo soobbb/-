72d6233f09965dda72dd75ed4ace10f805151631
soobbb <jsb8437@naver.com>
2023-10-12 17:31:37 +0900

    [study] map 배열 메서드

---
+++ 27장 배열/36.Array.prototype.map.js
@@ -0,0 +1,12 @@
+const numbers = [1, 4, 9];
+
+// map 메서드는 numbers 배열의 모든 요소를 순회하면서 콜백 함수를 반복 호출한다.
+// 콜백 함수의 반환값들로 구성된 새로운 배열을 반환한다.
+
+const roots = numbers.map((item) => Math.sqrt(item));
+
+// map 메서드는 새로운 배열을 반환한다.
+console.log(roots); // [ 1, 2, 3 ]
+
+// map 메서드는 새로운 배열을 변경하지 않는다.
+console.log(numbers); // [1, 4, 9]
