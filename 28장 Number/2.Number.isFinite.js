2ed2eefee4d62a2045038f46c618904e794f2134
soobbb <jsb8437@naver.com>
2023-10-17 17:31:31 +0900

    [study] Number.isFinite

---
+++ 28장 Number/2.Number.isFinite.js
@@ -0,0 +1,9 @@
+// 인수가 정상적인 유한수이면 true를 반환한다.
+
+Number.isFinite(0); // true
+Number.isFinite(Number.MAX_VALUE); // true
+Number.isFinite(Number.MIN_VALUE); // true
+
+// 인수가 무한수이면 false 반환한다.
+Number.isFinite(Infinity); // false
+Number.isFinite(-Infinity); // false
