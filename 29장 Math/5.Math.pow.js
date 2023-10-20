0c9ebd6e31cebca9d748ac957cb32ee5943b1ebe
soobbb <jsb8437@naver.com>
2023-10-20 21:59:18 +0900

    [study] pow max min메서드

---
+++ 29장 Math/5.Math.pow.js
@@ -0,0 +1,11 @@
+// 제곱
+console.log(Math.pow(2, 8)); // 256
+console.log(Math.pow(Math.pow(2, 2), 2)); // 16
+
+// 최댓값
+console.log(Math.max(1, 2, 3)); // 3
+console.log(Math.max(...[1, 2, 3])); // 배열 최댓값 찾는 방법
+
+// 최솟값
+console.log(Math.min(1,2,3)); // 1
+console.log(Math.min(...[1, 2, 3])); // 배열 최솟값 찾는 방법
